fc829d2bf3eeee3a1d08ed2ee225e2182c2d00aa
Naufal.alg <ahmadnaufalalghifari6@gamil.com>
2025-03-11 07:15:42 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4
+{
+    internal class KodeProduk
+    {
+        public enum produkElektronik
+        {
+            LAPTOP, SMARTPHONE, TABLET, HEADSET, KEYBOARD, MOUSE, PRINTER, MONITOR, SMARTWATCH, KAMERA
+        };
+        public string[] kodeProduk =
+        {
+            "E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109"
+        };
+
+        public string GetKodeProduk(produkElektronik i)
+        {
+            return kodeProduk[(int) i];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul4;
+
+class Program
+{
+    static void Main (string [] args) 
+    {
+        KodeProduk kodeProduk = new KodeProduk();
+        int i = 0;
+
+        Console.WriteLine(
+            $"Kode laptop = {kodeProduk.GetKodeProduk(KodeProduk.produkElektronik.LAPTOP)} \n" +
+            $"Kode printer = {kodeProduk.GetKodeProduk(KodeProduk.produkElektronik.PRINTER)} \n" +
+            $"Kode Kamera = {kodeProduk.GetKodeProduk(KodeProduk.produkElektronik.KAMERA)} \n" +
+            $"Kode Mouse = {kodeProduk.GetKodeProduk(KodeProduk.produkElektronik.MOUSE)}");
+
+    }
+}
+
